1d5a24d671ddf933b7492a6289d828c41032e0d4
dmdijumathew22 <dmdijumathew22@gmail.com>
2021-02-04 17:32:30 -0500

    servlet initializer needed only if we are packaging as war and deploying in external server

--- src/main/java/com/feedback/ServletInitializer.java
+++ src/main/java/com/feedback/ServletInitializer.java
@@ -1,13 +1,17 @@
-package com.feedback;
+/*
+ * package com.feedback;
+ * 
+ * import org.springframework.boot.builder.SpringApplicationBuilder; import
+ * org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+ * 
+ * public class ServletInitializer extends SpringBootServletInitializer {
+ * 
+ * @Override protected SpringApplicationBuilder
+ * configure(SpringApplicationBuilder application) { return
+ * application.sources(FeedbackApplication.class); }
+ * 
+ * }
+ */
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(FeedbackApplication.class);
-	}
-
-}
+//NEEDED IF WE ARE DEPLOYING THE PROJECT AS WAR IN EXTERNAL TOMCAT PROJECT
